b8574be9f8872852b94aee1f8c14913a021d24b8
nalin0503 <sharmanalin03@gmail.com>
2025-03-02 00:47:20 +0800

    add trump biden morph cmd

--- run.sh
+++ run.sh
@@ -5,9 +5,17 @@
 #     --output_path "./results/vangogh_pearlgirl" --use_adain --use_reschedule \
 #     --save_inter --use_lcm 
 
+# srun -p rtx3090_slab -w slabgpu08 --gres=gpu:1 \
+#     --job-name=test --kill-on-bad-exit=1 python3 main_lcm.py \
+#     --image_path_0 ./assets/lion.png --image_path_1 ./assets/tiger.png \
+#     --prompt_0 "A photo of a lion" --prompt_1 "A photo of a tiger" \
+#     --output_path "./results/lion_tiger" --use_adain --use_reschedule \
+#     --save_inter --use_lcm
+
 srun -p rtx3090_slab -w slabgpu08 --gres=gpu:1 \
     --job-name=test --kill-on-bad-exit=1 python3 main_lcm.py \
-    --image_path_0 ./assets/lion.png --image_path_1 ./assets/tiger.png \
-    --prompt_0 "A photo of a lion" --prompt_1 "A photo of a tiger" \
-    --output_path "./results/lion_tiger" --use_adain --use_reschedule \
+    --image_path_0 ./assets/trump.png --image_path_1 ./assets/biden.png \
+    --prompt_0 "A photo of an American man" --prompt_1 "A photo of an American man" \
+    --output_path "./results/Trump_Biden" --use_adain --use_reschedule \
     --save_inter --use_lcm
+
