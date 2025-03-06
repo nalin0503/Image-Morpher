3b4ccfadd22e1dc0f4212094811252cb7b3aa619
nalin0503 <sharmanalin03@gmail.com>
2025-03-07 02:37:42 +0800

    rmv run2.sh

--- run2.sh
+++
@@ -1,5 +0,0 @@
-# srun -p rtx3090_slab -w slabgpu08 --gres=gpu:1 \
-#     --job-name=test --kill-on-bad-exit=1 python3 lcm_lora_test.py
-
-srun -p rtx3090_slab -w slabgpu08 --gres=gpu:1 \
-    --job-name=test --kill-on-bad-exit=1 python3 FILM.py
