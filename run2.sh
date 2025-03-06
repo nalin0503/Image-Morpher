f3ca9864e0b7131cc773dbc53abaae38cf6a90e1
nalin0503 <sharmanalin03@gmail.com>
2025-03-07 02:13:48 +0800

    add film cmd

--- run2.sh
+++ run2.sh
@@ -1,2 +1,5 @@
+# srun -p rtx3090_slab -w slabgpu08 --gres=gpu:1 \
+#     --job-name=test --kill-on-bad-exit=1 python3 lcm_lora_test.py
+
 srun -p rtx3090_slab -w slabgpu08 --gres=gpu:1 \
-    --job-name=test --kill-on-bad-exit=1 python3 lcm_lora_test.py
+    --job-name=test --kill-on-bad-exit=1 python3 FILM.py
